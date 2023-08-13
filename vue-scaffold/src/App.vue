6dca3efa21f725f82357d7fde0f4ab21798396ca
wanghe <565817126@qq.com>
2023-08-13 08:58:07 +0800

    分析脚手架

--- vue-scaffold/src/App.vue
+++ vue-scaffold/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<h1 v-text="msg" ref="title"></h1>
+		<button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
+		<School ref="sch"/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	//引入School组件
+	import School from './components/School'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name:'App',
+		components:{School},
+		data() {
+			return {
+				msg:'欢迎学习Vue！'
+			}
+		},
+		methods: {
+			showDOM(){
+				console.log(this.$refs.title) //真实DOM元素
+				console.log(this.$refs.btn) //真实DOM元素
+				console.log(this.$refs.sch) //School组件的实例对象（vc）
+			}
+		},
+	}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
